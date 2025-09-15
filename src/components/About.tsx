5313e08272c9ae54a3b4a4ce73426b2eed9414b3
Abhishek Patil <sai28abhishekpatil@gmail.com>
2025-09-15 19:23:21 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -62,7 +62,7 @@ const About = () => {
                 <CardContent className="p-6 text-center">
                   <div className="mb-4 flex justify-center">
                     <div className="p-3 bg-gradient-hero rounded-lg group-hover:scale-110 transition-transform duration-300">
-                      <feature.icon className="h-8 w-8 text-black"/>
+                      <feature.icon className="h-8 w-8 text-primary"/>
                     </div>
                   </div>
                   <h4 className="text-lg font-semibold mb-3">{feature.title}</h4>
